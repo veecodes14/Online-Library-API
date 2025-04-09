e9d963629f18958980a17f82e3268a987d377c2b
veecodes14 <134261808+veecodes14@users.noreply.github.com>
2025-04-09 21:07:57 +0000

    updating progress

---
+++ library/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+
+
+def home (request):
+    content = {'posts': Post.objects.all()}
+    return render(request, 'library/home.html', content)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'library/home.html' #<app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object
+        if self.request.user == post.author:
+            return True
+        return False
+        #return super().test_func()
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+
+    #def test_func(self):
+        #post = self.get_object()
+        #return self.request.user == post.author
+
+
+        #if self.request.user == post.author:
+            #return True
+        #return False
+
+    
+
+def about(request):
+    return render(request, 'library/about.html', {'title': 'About'})
+# Create your views here.
