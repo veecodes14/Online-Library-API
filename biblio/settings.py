1323d465241cbcd5f1313b4c8e0fb99004ee298f
veecodes14 <134261808+veecodes14@users.noreply.github.com>
2025-09-13 14:20:07 +0000

    host name added

--- biblio/settings.py
+++ biblio/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c^_!x!=y(=&e@$_jf%hrbqd3lkowmjgewqma&%^v&aqv==i)(!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'online-library-api.onrender.com']
 
 
 # Application definition
